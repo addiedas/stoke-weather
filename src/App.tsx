43d62de2f9c0b8cfc648260865f122d9880d2e03
Addie Cohen <addie@swimm.io>
2021-11-07 15:46:30 +0200

    removed logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './icons/day-and-night.png';
 import {SearchBar} from './features/search-bar/search-bar';
 import './App.css';
 import {ComponentSwitch} from './features/component-switch/component-switch';
@@ -8,7 +7,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <SearchBar/>
                 <ComponentSwitch/>
             </header>
